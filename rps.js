156b6bfbdd0c26675997a6d1c498c9d024275e8e
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2023-12-30 12:01:19 -0800

    Updated game to have UI

--- rps.js
+++ rps.js
@@ -1,4 +1,38 @@
-let score = 0;
+const btn = document.querySelectorAll(".btn");
+
+// specific buttons (unneeded)
+// const rockButton = document.querySelector(".btn-rock");
+// const paperButton = document.querySelector(".btn-paper");
+// const scissorButton = document.querySelector(".btn-scissor");
+
+// Text on DOM
+const textPlayer = document.querySelector(".para-player");
+const textComputer = document.querySelector(".para-computer");
+const textResult = document.querySelector(".para-result");
+const textScore = document.querySelector(".para-score");
+
+btn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const playerSelection = e.target.textContent;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection.toLowerCase(), computerSelection);
+    textScore.textContent = `Your score is ${playerScore} and Computer is ${computerScore}`;
+
+    if (playerScore === 5) {
+      textScore.textContent = `Player has won! Lets try again!`;
+      playerScore = 0;
+      computerScore = 0;
+    }
+    if (computerScore === 5) {
+      textScore.textContent = `Computer has won! Lets play again!`;
+      playerScore = 0;
+      computerScore = 0;
+    }
+  });
+});
+
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let choice = Math.ceil(Math.random() * 3);
@@ -13,33 +47,42 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  console.log("Player chose: " + playerSelection);
-  console.log("Computer chose: " + computerSelection);
-  if (playerSelection === computerSelection) return console.log("It's a tie!");
-  else if (playerSelection === "rock") {
+  textPlayer.textContent = "Player chose: " + playerSelection;
+  textComputer.textContent = "Computer chose: " + computerSelection;
+  if (playerSelection === computerSelection)
+    return (textResult.textContent = "It's a tie!");
+  else if (playerSelection === "rock") { // Player Rock
+
     if (computerSelection === "scissors") {
-      score++;
-      return console.log("You win!");
-    } else return console.log("You lose :(");
-  }
-  else if (playerSelection === "paper") {
+      playerScore++;
+      return (textResult.textContent = "You win!");
+    } else {
+      computerScore++ 
+      return textResult.textContent = "You lose :("
+    }
+  } else if (playerSelection === "paper") { // Player Paper
+
     if (computerSelection === "rock") {
-      score++;
-      return console.log("You win!");
-    } else return console.log("You lose :(");
-  }
-  else if (playerSelection === "scissors") {
+      playerScore++;
+      return (textResult.textContent = "You win!");
+    } else {
+      computerScore++ 
+      return textResult.textContent = "You lose :("
+    }
+
+  } else if (playerSelection === "scissors") { // Player Scissor
     if (computerSelection === "paper") {
-      score++;
-      return console.log("You win!");
-    } else return console.log("You lose :(");
+      playerScore++;
+      return (textResult.textContent = "You win!");
+    } else {
+      computerScore++ 
+      return textResult.textContent = "You lose :("
+    }
   }
 }
 
-for (let i = 0; i < 5; i++) {
-  const playerSelection = prompt();
-  const computerSelection = getComputerChoice();
-  playRound(playerSelection.toLowerCase(), computerSelection);
-}
-
-console.log("Your score is " + score);
+// for (let i = 0; i < 5; i++) {
+// const playerSelection = prompt();
+// const computerSelection = getComputerChoice();
+// playRound(playerSelection.toLowerCase(), computerSelection);
+// }
